c7cb740dcb56a8c385a887f0bcdac9632817d647
davidhdzmon <davidhdzmon@gmail.com>
2018-01-11 18:18:22 +0100

    Archivo readme editado, clase java editada y compilada

--- HolaMundoDavid.java
+++ HolaMundoDavid.java
@@ -1,7 +1,7 @@
 public class HolaMundoDavid {
 
 	public static void main (String[]args){
-		String holaMundo="Prueba inicial de repositorio en github, David Hernandez Monforte. Hola mundo!!";
+		String holaMundo="Prueba de edicion en repositorio de github, David Hernandez Monforte. Hola mundo editado!!";
 
 		System.out.println(holaMundo);
 	}
