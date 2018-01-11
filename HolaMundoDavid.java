f8a03d295af80f23b6300f01f0026c5a75af57fe
davidhdzmon <davidhdzmon@gmail.com>
2018-01-11 17:58:55 +0100

    Primera subida desde consola

---
+++ HolaMundoDavid.java
@@ -0,0 +1,9 @@
+public class HolaMundoDavid {
+
+	public static void main (String[]args){
+		String holaMundo="Prueba inicial de repositorio en github, David Hernandez Monforte. Hola mundo!!";
+
+		System.out.println(holaMundo);
+	}
+
+}
